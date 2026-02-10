be2353d6d59b17cb3ea1184e94d2d0b5d592b3e4
huy14032003 <trhuy14032003@gmail.com>
2026-02-11 00:28:27 +0700

    update phim

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,6 +6,12 @@ const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   // Override default ignores of eslint-config-next.
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
